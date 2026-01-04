70afdf7d76dc06ed35300a529cac555cd3ec4634
Float07 <thiago.d.c.de.pauli@gmail.com>
2026-01-04 11:40:57 -0300

    Implement initial code for block health and level

--- src/block.py
+++ src/block.py
@@ -5,11 +5,16 @@ class Block:
   blockWidth = 32
   blockHeight = 16
 
-  def __init__(self, x, y):
+  def __init__(self, x, y, blockLvl=1):
     self.relX = x
     self.relY = y
     self.x = x * Block.blockWidth
     self.y = y * Block.blockHeight
+    self.blockLvl = blockLvl
+    self.health = blockLvl
+
+  def takeDamage(self):
+    self.health -= 1
   
   def draw(self):
     blockWidth = Block.blockWidth
